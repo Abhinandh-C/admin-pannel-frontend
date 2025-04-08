3f93734c58fe7771cdd2ec54609602fbe98de42f
Abhinandh <abhinandh20011@gmail.com>
2025-04-08 17:37:42 +0530

    app js created and added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Header from './components/Header/Header';
+
 
 function App() {
  
 
   return (
     <>
-     <h1>DashBoard</h1>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+       <Route path="/" exact={true} element={<Dashboard/>} />
+       <Route path="/Dashboard" exact={true} element={<Dashboard/>} />
+    </Routes>
+    
+    </BrowserRouter>
     </>
   )
 }
