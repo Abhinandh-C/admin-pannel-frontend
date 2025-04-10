d4768b635e3059fc988ff679c04d35b5ceafa545
Abhinandh <abhinandh20011@gmail.com>
2025-04-10 12:31:57 +0530

    profile picture has been added and style has been updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,12 @@ import Button from '@mui/material/Button';
 import { MdOutlineMenuOpen } from "react-icons/md";
 import { MdOutlineMenu } from "react-icons/md";
 import SearchBox from '../search box/SearchBox';
+import { MdLightMode } from "react-icons/md";
+import { FaMoon } from "react-icons/fa";
+import { MdNotificationsActive } from "react-icons/md";
+import { IoMailUnread } from "react-icons/io5";
+import { IoIosCart } from "react-icons/io";
+import pic from "../../images/pic.jpg"
 
 const Header = () => {
   return (
@@ -25,6 +31,26 @@ const Header = () => {
             <SearchBox/>
             </div>
 
+            <div className="col-sm-7 d-flex align-items-center part3 justify-content-end">
+            <Button className='rounded-circle'><MdLightMode/></Button>
+            <Button className='rounded-circle'><IoIosCart/></Button>
+            <Button className='rounded-circle'><IoMailUnread/></Button>
+            <Button className='rounded-circle'><MdNotificationsActive/></Button>
+
+
+
+            <div className="imgWrapper">
+            <div className="myAcc d-flex align-items-center">
+              <div className="userImg">
+                <span className='rounded-circle'>
+                  <img src={pic} alt="profilePic"  />
+                </span>
+              </div>
+
+            </div>
+            </div>
+
+            </div>
 
 
 
