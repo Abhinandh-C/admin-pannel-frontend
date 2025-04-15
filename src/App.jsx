860640d49e528a049af87b489755f132ebc0f670
Abhinandh <abhinandh20011@gmail.com>
2025-04-15 16:23:23 +0530

    added sidebar component in app.jsx and align the into main and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Header from './components/Header/Header';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
- 
-
   return (
     <>
-    <BrowserRouter>
-    <Header />
-    <Routes>
-       <Route path="/" exact={true} element={<Dashboard/>} />
-       <Route path="/Dashboard" exact={true} element={<Dashboard/>} />
-    </Routes>
-    
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <div className="main d-flex">
+          <div className="sidebar-Wrapper">
+            <Sidebar />
+          </div>
+          <div className="content">
+            <Routes>
+              <Route path="/" exact={true} element={<Dashboard />} />
+              <Route path="/Dashboard" exact={true} element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
