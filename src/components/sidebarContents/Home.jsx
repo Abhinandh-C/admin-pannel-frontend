f97d22ed4db3580b31a109a2dd7f687c5a553491
Abhinandh <abhinandh20011@gmail.com>
2025-04-18 16:36:59 +0530

    home has been updated with link added to orders

--- src/components/sidebarContents/Home.jsx
+++ src/components/sidebarContents/Home.jsx
@@ -2,13 +2,17 @@ import React, { Fragment, useEffect } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { AiOutlineDoubleRight } from "react-icons/ai";
 import PieChart from './PieChart';
+// import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  useEffect(()=>{
-    console.log('hoemma]sfklfn');
-  
-    
-  })
+  useEffect(() => {
+    console.log('Home loaded');
+  }, []);
+
+
+
   return (
     <Fragment>
       <Container fluid className='d-flex mt-5'>
@@ -16,13 +20,19 @@ const Home = () => {
 
           <Col sm={12} md={3} lg={3}>
             <Card border="primary" className="bg-primary" style={{ width: '16rem' }}>
-              <Card.Body>
+              <Card.Body style={{ cursor: "pointer" }}>
                 <Card.Title className="display-2 text-white">150</Card.Title>
                 <Card.Text className="text-white">
                   New Orders
                 </Card.Text>
-                <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
               </Card.Body>
+              <Card.Footer>
+                <Link to="/orderList" className="text-white text-decoration-none">
+                 
+                 More info <AiOutlineDoubleRight />
+                
+                </Link>
+              </Card.Footer>
             </Card>
           </Col>
 
@@ -33,8 +43,8 @@ const Home = () => {
                 <Card.Text className="text-white">
                   Bounce Rate
                 </Card.Text>
-                <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
               </Card.Body>
+              <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
             </Card>
           </Col>
 
@@ -45,8 +55,8 @@ const Home = () => {
                 <Card.Text className="text-white">
                   User Registration
                 </Card.Text>
-                <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
               </Card.Body>
+              <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
             </Card>
           </Col>
 
@@ -57,17 +67,17 @@ const Home = () => {
                 <Card.Text className="text-white">
                   Unic Visitor
                 </Card.Text>
-                <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
               </Card.Body>
+              <Card.Footer className="text-white">More info <AiOutlineDoubleRight /></Card.Footer>
             </Card>
           </Col>
 
         </Row>
       </Container>
 
-      <div className="mt-3" style={{ marginBottom: "100px" }}>
-        <PieChart />
-      </div>
+      <div className="mt-3" style={{ marginBottom: "100px" }}></div>
+
+      <PieChart />
     </Fragment>
   );
 };
