f3660f9e4ad52d0c759f7644f27ae14482be1982
Abhinandh <abhinandh20011@gmail.com>
2025-04-18 16:41:58 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./components/sidebarContents/Home";
+import NewOrderTable from "./components/sidebarContents/NewOrderTable";
+import PieChart from "./components/sidebarContents/PieChart";
+import UserList from "./components/sidebarContents/UserList";
+import AddProduct from "./components/sidebarContents/AddProduct";
+import OrderManagement from "./components/sidebarContents/OrderManagement";
+import TodoList from "./components/sidebarContents/TodoList";
 
 
 function App() {
@@ -11,17 +18,29 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-        <div className="w-100">
-          <div className="sidebar-Wrapper">
+        <div className="d-flex">
+        <div>
+          
             <Sidebar />
           </div>
-          <div className="content">
+        
+            <div className="w-100 content" style={
+                {maxHeight:"720px",overflowX:"hidden",overflowY:"auto"}
+              }> 
             <Routes>
-              <Route path="/" exact={true} element={<Dashboard />} />
+              <Route path="/home" exact={true} element={<Home />} />
+              <Route path="/orderList" exact={true} element={<NewOrderTable/>} />
+              <Route path="/piechart" exact={true} element={<PieChart />} />
+              <Route path="/userinformation" exact={true} element={<UserList />} />
+              <Route path="/AddProduct" exact={true} element={<AddProduct />} />
+              <Route path="/ordermangement" exact={true} element={<OrderManagement />} />
+              <Route path="/todolist" exact={true} element={<TodoList />} />
+              <Route path="/Dashboard" exact={true} element={<Dashboard />} />
+              <Route path="/Dashboard" exact={true} element={<Dashboard />} />
               <Route path="/Dashboard" exact={true} element={<Dashboard />} />
             </Routes>
-          </div>
-        </div>
+            </div>
+            </div>
       </BrowserRouter>
     </>
   );
