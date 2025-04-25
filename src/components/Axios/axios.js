fba8687161a1a7f63d11c110d784a146c0e29725
Abhinandh <abhinandh20011@gmail.com>
2025-04-25 15:57:28 +0530

    Refactor : edited changes and added token in axios

--- src/components/Axios/axios.js
+++ src/components/Axios/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios"
 
+const token = localStorage.getItem("token");
 const Axios =axios.create({
 
      baseURL :"http://localhost:3000",
-     headers:{"Content-Type":"application-json"},
+     headers:{"Content-Type":"application/json",Authorization: `Bearer ${token}`},
      withCredentials: true 
 })
 
