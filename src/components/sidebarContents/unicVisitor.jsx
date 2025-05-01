16b91b2cae3edddc0223fb368b72b3def695105f
Abhinandh <abhinandh20011@gmail.com>
2025-05-01 17:15:28 +0530

    Feat : created unicVisitor jsx

---
+++ src/components/sidebarContents/unicVisitor.jsx
@@ -0,0 +1,74 @@
+import { Table } from 'bootstrap-4-react/lib/components';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Axios from '../Axios/axios';
+import { notifyError } from "../utilities/utilities";
+
+const UnicVisitor = () => {
+  const [tableData, setTableData] = useState([]);
+  const fetchOrders = async () => {
+    try {
+      const res = await Axios.get("/viewallorder", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+  
+      const data = res.data;
+      if (Array.isArray(data)) {
+        setTableData(data);
+      } else if (Array.isArray(data.orderdetails)) {
+        setTableData(data.orderdetails);
+      } else {
+        console.warn("Unexpected response structure:", data);
+        setTableData([]);
+      }
+    } catch (error) {
+      notifyError("No Order available");
+      console.error("Error fetching orders:", error);
+    }
+  };
+ 
+
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <Container className="mt-4">
+      <h4 className="mb-3">Visitors List</h4>
+      <Table responsive="sm" striped bordered hover>
+        <thead className="table-primary">
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((order, index) => (
+            <tr key={order.id || index}>
+              <td>{index + 1}</td>
+              <td>{order.address?.firstName}</td>
+              <td>{order.address?.email}</td>
+              <td>{order.address?.PhoneNumber}</td>
+              <td>{order.address?.address},{order.address?.city},{order.address?.state},</td>
+   
+
+
+
+            </tr>
+          ))}
+        </tbody>
+        
+      </Table>
+    </Container>
+    
+  );
+};
+
+export default UnicVisitor;
